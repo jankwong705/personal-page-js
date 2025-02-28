da0ad1a15f4083fed3ec9a3723708bb64e3111af
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-02-28 15:46:34 -0800

    fade

--- script.js
+++ script.js
@@ -8,26 +8,63 @@ document.addEventListener("DOMContentLoaded", () => {
     nameField.addEventListener("input", () => {
         const errorMsg = document.getElementById("name-pattern-mismatch");
         if (nameField.validity.patternMismatch) {
+            // Flash field 
+            
+
+            // Display error msg
             errorMsg.value = "(Special characters disallowed.)";
+            errorMsg.style.opacity = "1";
+            // Fade
+            setTimeout(() => {
+                errorMsg.style.transition = "opacity 1s ease-out";
+                errorMsg.style.opacity = "0";
+            }, 2000);
         }
-        else { errorMsg.value = ""; }
     });
 
     numField.addEventListener("input", () => {
+        const errorMsg = document.getElementById("num-pattern-mismatch");
         if (numField.validity.patternMismatch) {
-            const errorMsg = document.getElementById("num-pattern-mismatch");
+            //Flash
+            
+
+            // Display error msg
             errorMsg.value = "(Only numbers allowed.)";
+            errorMsg.style.opacity = "1";
+            // Fade
+            setTimeout(() => {
+                errorMsg.style.transition = "opacity 1s ease-out";
+                errorMsg.style.opacity = "0";
+            }, 2000);
         }
     });
 
     emailField.addEventListener("blur", () => {
+        const errorMsg = document.getElementById("email-pattern-mismatch");
         if (emailField.validity.patternMismatch) {
+            
+            // Display error msg
+            errorMsg.value = "(Not a valid email)";
+            errorMsg.style.opacity = "1";
+            // Fade
+            setTimeout(() => {
+                errorMsg.style.transition = "opacity 1s ease-out";
+                errorMsg.style.opacity = "0";
+            }, 2000);
         }
     });
 
     msgField.addEventListener("input", () => {
+        const errorMsg = document.getElementById("message-error");
         if (msgField.validity.patternMismatch) {
+            
+            errorMsg.value = "(Special characters disallowed.)";
         }
+        errorMsg.style.opacity = "1";
+        setTimeout(() => {
+            errorMsg.style.transition = "opacity 1s ease-out";
+            errorMsg.style.opacity = "0";
+        }, 2000);
     })
     
 });
