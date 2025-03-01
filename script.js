344adb163021bf233765e76ecc87e60ccd93a683
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-02-28 17:00:54 -0800

    minor update

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
         else {
             msgField.style.backgroundColor = "white";
         }
+        // Count char 
     });
     
 });
