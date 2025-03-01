ff66198fe738ce7c40f8809ef30608b1d1a351f4
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-02-28 16:19:19 -0800

    flash

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     nameField.addEventListener("input", () => {
         const errorMsg = document.getElementById("name-pattern-mismatch");
         if (nameField.validity.patternMismatch) {
-            // Flash field 
+            //Flash
+            setTimeout(() => {
+                nameField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
+            }, 100);
+            setTimeout(() => {
+                nameField.style.backgroundColor = "white" ? "rgb(243, 199, 199)" : "rgb(243, 199, 199)";
+            }, 100);
+            nameField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
             
-
             // Display error msg
             errorMsg.value = "(Special characters disallowed.)";
             errorMsg.style.opacity = "1";
@@ -20,13 +26,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 errorMsg.style.opacity = "0";
             }, 2000);
         }
+        else {
+            nameField.style.backgroundColor = "white";
+        }
     });
 
     numField.addEventListener("input", () => {
         const errorMsg = document.getElementById("num-pattern-mismatch");
         if (numField.validity.patternMismatch) {
             //Flash
-            
+            setTimeout(() => {
+                numField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
+            }, 100);
+            setTimeout(() => {
+                numField.style.backgroundColor = "white" ? "rgb(243, 199, 199)" : "rgb(243, 199, 199)";
+            }, 100);
+            numField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
 
             // Display error msg
             errorMsg.value = "(Only numbers allowed.)";
@@ -37,12 +52,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 errorMsg.style.opacity = "0";
             }, 2000);
         }
+        else {
+            numField.style.backgroundColor = "white";
+        }
     });
 
     emailField.addEventListener("blur", () => {
         const errorMsg = document.getElementById("email-pattern-mismatch");
         if (emailField.validity.patternMismatch) {
-            
+            //Flash
+            setTimeout(() => {
+                emailField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
+            }, 100);
+            setTimeout(() => {
+                emailField.style.backgroundColor = "white" ? "rgb(243, 199, 199)" : "rgb(243, 199, 199)";
+            }, 100);
+            emailField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
+
             // Display error msg
             errorMsg.value = "(Not a valid email)";
             errorMsg.style.opacity = "1";
@@ -52,19 +78,32 @@ document.addEventListener("DOMContentLoaded", () => {
                 errorMsg.style.opacity = "0";
             }, 2000);
         }
+        else {
+            emailField.style.backgroundColor = "white";
+        }
     });
 
     msgField.addEventListener("input", () => {
         const errorMsg = document.getElementById("message-error");
         if (msgField.validity.patternMismatch) {
+            //Flash
+            setTimeout(() => {
+                msgField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
+            }, 100);
+            setTimeout(() => {
+                msgField.style.backgroundColor = "white" ? "rgb(243, 199, 199)" : "rgb(243, 199, 199)";
+            }, 100);
+            msgField.style.backgroundColor = "rgb(243, 199, 199)" ? "white" : "white";
             
+            // Display error msg
             errorMsg.value = "(Special characters disallowed.)";
+            errorMsg.style.opacity = "1";
+            // Fade
+            setTimeout(() => {
+                errorMsg.style.transition = "opacity 1s ease-out";
+                errorMsg.style.opacity = "0";
+            }, 2000);
         }
-        errorMsg.style.opacity = "1";
-        setTimeout(() => {
-            errorMsg.style.transition = "opacity 1s ease-out";
-            errorMsg.style.opacity = "0";
-        }, 2000);
     })
     
 });
