afc237532d337408b0af5282b2732579f2afe5bf
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-01 16:58:11 -0800

    style

--- theme.js
+++ theme.js
@@ -1,8 +1,14 @@
 const lightSwtich = document.getElementById("light-switch");
 
+if(localStorage.getItem("mode") == "dark") {
+    document.body.classList.add("dark-mode");
+    lightSwtich.checked = false;
+}
+
 lightSwtich.addEventListener("change", () => {
     // Light mode
     if(lightSwtich.checked) {
+        document.body.classList.remove("dark-mode");
         localStorage.setItem("mode", "light");
     }
     // Dark mode
