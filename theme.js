9eacb8946649fb80cb60a42404fbd8b6ec8f5dfa
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-01 16:43:52 -0800

    no js no display

---
+++ theme.js
@@ -0,0 +1,10 @@
+const lightSwtich = document.getElementById("light-switch");
+
+lightSwtich.addEventListener("change", () => {
+    if(lightSwtich.checked) {
+        // Light mode
+    }
+    else {
+        // Dark mode
+    }
+});
