f2fc0d6228baba40719360063f5a981fc1050458
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-02 01:18:45 -0800

    dark

--- theme.js
+++ theme.js
@@ -1,19 +1,32 @@
+const darkMode = "dark-mode";
+
 const lightSwtich = document.getElementById("light-switch");
+const expList = document.getElementsByTagName("job-exp");
+
 
 if(localStorage.getItem("mode") == "dark") {
-    document.body.classList.add("dark-mode");
     lightSwtich.checked = false;
+    document.body.classList.add(darkMode);
+    for (let exp of expList) {
+        exp.classList.add(darkMode);
+    }
 }
 
 lightSwtich.addEventListener("change", () => {
     // Light mode
     if(lightSwtich.checked) {
-        document.body.classList.remove("dark-mode");
+        document.body.classList.remove(darkMode);
+        for (let exp of expList) {
+            exp.classList.remove(darkMode);
+        }
         localStorage.setItem("mode", "light");
     }
     // Dark mode
     else {
-        document.body.classList.add("dark-mode");
+        document.body.classList.add(darkMode);
+        for (let exp of expList) {
+            exp.classList.add(darkMode);
+        }
         localStorage.setItem("mode", "dark");
     }
 });
