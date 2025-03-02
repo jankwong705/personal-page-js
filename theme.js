707980ebde0ee078df2e2c41aa851948d84258f7
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-02 01:44:50 -0800

    dark m

--- theme.js
+++ theme.js
@@ -2,31 +2,33 @@ const darkMode = "dark-mode";
 
 const lightSwtich = document.getElementById("light-switch");
 const expList = document.getElementsByTagName("job-exp");
+const headerList = document.querySelectorAll('header ul li');
+const refList = document.querySelectorAll("a");
 
 
 if(localStorage.getItem("mode") == "dark") {
     lightSwtich.checked = false;
     document.body.classList.add(darkMode);
-    for (let exp of expList) {
-        exp.classList.add(darkMode);
-    }
+    for (let exp of expList) { exp.classList.add(darkMode); }
+    for (let item of headerList ) { item.classList.add(darkMode); }
+    for (let a of refList) { a.classList.add(darkMode); }
 }
 
 lightSwtich.addEventListener("change", () => {
     // Light mode
     if(lightSwtich.checked) {
         document.body.classList.remove(darkMode);
-        for (let exp of expList) {
-            exp.classList.remove(darkMode);
-        }
+        for (let exp of expList) { exp.classList.remove(darkMode); }
+        for (let item of headerList ) { item.classList.remove(darkMode); }
+        for (let a of refList) { a.classList.remove(darkMode); }
         localStorage.setItem("mode", "light");
     }
     // Dark mode
     else {
         document.body.classList.add(darkMode);
-        for (let exp of expList) {
-            exp.classList.add(darkMode);
-        }
+        for (let exp of expList) { exp.classList.add(darkMode); }
+        for (let item of headerList ) { item.classList.add(darkMode); }
+        for (let a of refList) { a.classList.add(darkMode); }
         localStorage.setItem("mode", "dark");
     }
 });
